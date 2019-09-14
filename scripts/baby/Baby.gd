00fbfd36ced119bc290965af7926b940517c8881
James T <github.jtara1@gmail.com>
2019-09-14 15:34:58 -0700

    refactor: make SlimeSprite its own tscn for baby & big boi can use it

--- scripts/baby/Baby.gd
+++ scripts/baby/Baby.gd
@@ -2,15 +2,11 @@ extends Area2D # extends something else
 
 var died = false
 
-func _enter_tree():
-	$Character.speed = 0
-	$Character.set_collision_layer_bit(0, 0)
-	$Character.set_collision_mask_bit(0, 0)
 
 func die():
 	died = true
 	$AudioStreamPlayer2D.play()
-	$Character/AnimationPlayer.play("death")
+	$SlimeSprite/AnimationPlayer.play("death")
 
 ######################
 # event listners
