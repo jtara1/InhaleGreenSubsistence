4a6160c3ec0d67b7a64e91bddd573224c7092145
James T <github.jtara1@gmail.com>
2019-09-14 12:26:46 -0700

    add: proper lerp in scaling big boi slime up

--- scripts/character/Character.gd
+++ scripts/character/Character.gd
@@ -1,13 +1,6 @@
 extends "res://scripts/agent/Agent.gd" # extends KinematicBody2D
 
 export(float) var speed = 300.0
-export(float) var scaling_smoothing = 10
-
-onready var init_sprite_scale = $Sprite.scale
-onready var init_hurtbox_scale = $CollisionShape2D.scale
-
-onready var target_sprite_scale = init_sprite_scale
-onready var target_hurtbox_scale = init_hurtbox_scale
 
 
 func _ready():
@@ -15,7 +8,7 @@ func _ready():
 
 func _physics_process(delta):
 	move(delta)
-	scale_size(delta)
+	$CharacterScaling.scale_size(delta)
 	
 ####################
 # methods
@@ -33,26 +26,12 @@ func move(delta):
 		
 	position += movement * speed * delta
 	
-func scale_size(delta):
-	var sprite_scale = $Sprite.scale.slerp(target_sprite_scale, scaling_smoothing * delta)
-	var hbox_scale = $CollisionShape2D.scale.slerp(target_hurtbox_scale, scaling_smoothing * delta)
-	
-	if sprite_scale != Vector2.ZERO:
-		print(sprite_scale, hbox_scale)
-		$Sprite.scale += sprite_scale
-		$CollisionShape2D.scale += hbox_scale
-	
-
-func set_body_scaling(scaling_multiplier):
-	target_sprite_scale = $Sprite.scale * scaling_multiplier
-	target_hurtbox_scale = $CollisionShape2D.scale * scaling_multiplier
-	
 ####################
 # event listeners
 func _agent_consumed(attributes_mutated):
 	for attribute in attributes_mutated:
 		match attribute:
 			"body_size":
-				set_body_scaling(self.body_size)
+				$CharacterScaling.set_body_scaling(self.body_size)
 			"health":
 				pass # TODO: emit health changed signal for some UI 
