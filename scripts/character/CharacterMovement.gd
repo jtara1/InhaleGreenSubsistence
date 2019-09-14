d18575e043a692418d5e1abaf78aeaec669c8378
James T <github.jtara1@gmail.com>
2019-09-14 00:18:22 -0700

    add: basic wasd scripted movement

--- scripts/character/CharacterMovement.gd
+++ scripts/character/CharacterMovement.gd
@@ -2,11 +2,17 @@ extends KinematicBody2D
 
 export(float) var speed = 300.0
 
+
 func _physics_process(delta):
 	var movement = Vector2()
 	
 	if Input.is_action_pressed("move_right"):
 		movement += Vector2.RIGHT
+	if Input.is_action_pressed("move_down"):
+		movement += Vector2.DOWN
+	if Input.is_action_pressed("move_left"):
+		movement += Vector2.LEFT
+	if Input.is_action_pressed("move_up"):
+		movement += Vector2.UP
 		
-	print(movement)
 	position += movement * speed * delta
