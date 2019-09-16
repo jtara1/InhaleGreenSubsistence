12aca4999ba7bb5970ace6b5f00ad4a1c9b584b3
James T <github.jtara1@gmail.com>
2019-09-16 00:04:57 -0700

    add: killzone base tscn add: sawblade kz add: flame kz add: anims for ea kz add: their usage in lvl 1 rm: unnecessary scripting stuff

--- scripts/Killzone.gd
+++ scripts/Killzone.gd
@@ -1,15 +1,7 @@
 extends Area2D
 
-export(bool) var enable_sprite = true
-
 onready var Agent = load("res://scripts/agent/Agent.gd")
 
-
-func _ready():
-	if not enable_sprite:
-		$Sprite.visible = false
-	else:
-		$Sprite/AnimationPlayer.play("idle")
 
 func _on_Killzone_body_entered(body):
 	if body is Agent:
