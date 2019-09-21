5be064abe63e482c91a97dbab1bc805655b077b8
James T <github.jtara1@gmail.com>
2019-09-20 21:51:55 -0700

    add: baby randomly jump and cries every 15 secs

--- scripts/baby/Baby.gd
+++ scripts/baby/Baby.gd
@@ -1,17 +1,23 @@
 extends Area2D # extends something else
 
+export(float) var chance_to_cry = 0.50
+export(float) var jump_height = -350
+
 var velocity = Vector2()
 var grounded = false
 var died = false
 
-
 func _ready():
+	jump()
 	$SlimeSprite/AnimationPlayer.connect("animation_finished", self, "_on_SlimSprite_animation_finished")
 	
 func _physics_process(delta):
-	if not grounded:
-		velocity += gravity * gravity_vec * delta
-		position += velocity * delta
+	velocity += (gravity if not grounded else 0) * gravity_vec * delta
+	position += velocity * delta
+
+func jump():
+	grounded = false
+	velocity += Vector2(0, jump_height)
 
 func die():
 	died = true
@@ -28,4 +34,11 @@ func _on_SlimSprite_animation_finished(anim_name):
 
 func _on_Baby_body_entered(body):
 	if body is TileMap:
+		velocity = Vector2.ZERO
 		grounded = true
+
+
+func _on_IdleTimer_timeout():
+	if randf() <= chance_to_cry:
+		$IdleAudioPlayer.play()
+		jump()
