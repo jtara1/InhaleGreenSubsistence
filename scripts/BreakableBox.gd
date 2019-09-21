63359625b1971310e9332efd3dd5a5bc5c5c9a2d
James T <github.jtara1@gmail.com>
2019-09-21 00:12:14 -0700

    chg: box req char to be this big

--- scripts/BreakableBox.gd
+++ scripts/BreakableBox.gd
@@ -1,15 +1,19 @@
 extends "res://scripts/GameObject.gd" # extends Area2D
 
+export(int) var size_needed_to_break = 2
+
 
 func break_self():
 	if not died:
+		$KinematicBody2D.queue_free()
 		die()
 		$AnimatedSprite.playing = true
 		$AudioStreamPlayer2D.playing = true
 
 func _on_BreakableBox_body_entered(body):
 	if body is Agent:
-		break_self()
+		if body.body_size >= size_needed_to_break:
+			break_self()
 
 func _on_AnimatedSprite_animation_finished():
 	queue_free()
