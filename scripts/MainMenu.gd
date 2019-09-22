c9a6ba2a3f421014b927df1c25de7c0dae2acaaf
James T <github.jtara1@gmail.com>
2019-09-21 21:39:29 -0700

    chg: main menu music; add: modals with info in main menu

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -6,5 +6,11 @@ onready var Scene = get_node("/root/Scene")
 func _on_Start_pressed():
 	Scene.load_scene("Level1")
 
+func _on_About_pressed():
+	$WindowDialog.popup_centered_ratio(0.8)
+	
 func _on_Quit_pressed():
 	get_tree().quit()
+
+func _on_Credits_pressed():
+	$CreditsDialog.popup_centered_ratio(0.8)
