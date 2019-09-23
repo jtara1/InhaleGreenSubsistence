afb386a44463d7426cb9d31625a99d64efde86a5
James T <github.jtara1@gmail.com>
2019-09-22 17:18:06 -0700

    add: lvl3 to Scene script

--- scripts/globals/Scene.gd
+++ scripts/globals/Scene.gd
@@ -8,6 +8,7 @@ const Paths = {
 	"Level1": "res://scenes/Level1.tscn", 
 	"Lv1": "res://scenes/Lv1.tscn", 
 	"Lv2": "res://scenes/Lv2.tscn",
+	"Lv3": "res://scenes/Lv3.tscn",
 }
 
 func load_scene(scene):
